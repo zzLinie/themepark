15e469cd63288c85836f85fddec9a4f0a62e7362
Darrenf040 <darrenfr83@gmail.com>
2024-10-14 20:40:23 -0500

    react router to navigate to website pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./pages/Home";
 import "./index.css";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Home />,
+    errorElement: <div>404 Page Not Found</div>,
+  },
+]);
+
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <Home />
+    <RouterProvider router={router} />
   </StrictMode>
 );
