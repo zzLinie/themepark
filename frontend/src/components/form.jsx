64e1150d5089be7d0bf1b45c6e834ffc29ad3ad6
Darrenf040 <darrenfr83@gmail.com>
2024-10-17 09:56:56 -0500

    user form input saved inside a useState variable

--- frontend/src/components/form.jsx
+++ frontend/src/components/form.jsx
@@ -1,4 +1,5 @@
 import "./form.css";
+import { useState } from "react";
 
 export default function Form({
   heading,
@@ -7,15 +8,32 @@ export default function Form({
   label2Text,
   submitText,
 }) {
+  const [userName, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const userInput = (e) => {
+    //update userName variable as user types
+    setUserName(e.target.value);
+  };
+  const passwordInput = (e) => {
+    //update password variable as user types
+    setPassword(e.target.value);
+  };
+  const getInput = (e) => {
+    e.preventDefault();
+    return {
+      user: userName,
+      password: password,
+    };
+  };
   return (
     <div className="form-container">
       <h1>{heading}</h1>
       <p>{subheading}</p>
-      <form action="">
+      <form onSubmit={getInput}>
         <label htmlFor="">{label1Text}</label>
-        <input type="email" />
+        <input type="text" onChange={userInput} />
         <label htmlFor="">{label2Text}</label>
-        <input type="password" />
+        <input type="password" onChange={passwordInput} />
         <input type="submit" value={submitText} />
       </form>
     </div>
