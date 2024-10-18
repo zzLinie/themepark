bc5ebe0f8231bcb69f66d58b1b5b30abeec78505
Darrenf040 <darrenfr83@gmail.com>
2024-10-18 17:06:59 -0500

    removed use state and added userInput as a prop

--- frontend/src/components/form.jsx
+++ frontend/src/components/form.jsx
@@ -1,5 +1,4 @@
 import "./form.css";
-import { useState } from "react";
 
 export default function Form({
   heading,
@@ -7,33 +6,17 @@ export default function Form({
   label1Text,
   label2Text,
   submitText,
+  getUserInput,
 }) {
-  const [userName, setUserName] = useState("");
-  const [password, setPassword] = useState("");
-  const userInput = (e) => {
-    //update userName variable as user types
-    setUserName(e.target.value);
-  };
-  const passwordInput = (e) => {
-    //update password variable as user types
-    setPassword(e.target.value);
-  };
-  const getInput = (e) => {
-    e.preventDefault();
-    return {
-      user: userName,
-      password: password,
-    };
-  };
   return (
     <div className="form-container">
       <h1>{heading}</h1>
       <p>{subheading}</p>
-      <form onSubmit={getInput}>
+      <form onSubmit={getUserInput}>
         <label htmlFor="">{label1Text}</label>
-        <input type="text" onChange={userInput} />
+        <input type="text" />
         <label htmlFor="">{label2Text}</label>
-        <input type="password" onChange={passwordInput} />
+        <input type="password" />
         <input type="submit" value={submitText} />
       </form>
     </div>
