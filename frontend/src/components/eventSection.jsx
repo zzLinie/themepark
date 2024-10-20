d0550073a49f1c5690df083901f10543a8860ffc
Darrenf040 <darrenfr83@gmail.com>
2024-10-20 02:45:59 -0500

    event page

--- frontend/src/components/eventSection.jsx
+++ frontend/src/components/eventSection.jsx
@@ -1,3 +1,30 @@
-export default function EventSection() {
-  return <></>;
+import PropTypes from "prop-types";
+import "./eventSection.css";
+export default function EventSection({
+  eventName,
+  eventImage,
+  eventStart,
+  eventEnd,
+  eventDescription,
+}) {
+  return (
+    <section>
+      <div
+        className="event-container"
+        style={{ backgroundImage: `url(${eventImage})` }}
+      >
+        <h1>{eventName}</h1>
+        <p>{`${eventName} starts ${eventStart} & ends ${eventEnd}`}</p>
+        <p>{eventDescription}</p>
+      </div>
+    </section>
+  );
 }
+
+EventSection.propTypes = {
+  eventName: PropTypes.string,
+  eventStart: PropTypes.string,
+  eventEnd: PropTypes.string,
+  eventDescription: PropTypes.string,
+  eventImage: PropTypes.string,
+};
--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -1,3 +1,29 @@
+import Header from "../components/header";
+import EventSection from "../components/eventSection";
+import eventImage from "../assets/images/placeholder-image.webp";
+import "./events.css";
+
 export default function Events() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <div className="hero">
+        <h1>Special Events</h1>
+      </div>
+      <EventSection
+        eventName={"event 1"}
+        eventImage={eventImage}
+        eventStart={"October 20th"}
+        eventEnd={"October 30th"}
+        eventDescription={"October fest"}
+      />
+      <EventSection
+        eventName={"Event 2"}
+        eventDescription={"face your fears at our haunted park"}
+        eventEnd={"october 30th"}
+        eventStart={"october 25th"}
+        eventImage={eventImage}
+      />
+    </>
+  );
 }
