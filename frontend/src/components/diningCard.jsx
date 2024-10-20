cb29e2880d04d627f0204e8dd472a0443ddb1778
Darrenf040 <darrenfr83@gmail.com>
2024-10-20 01:47:17 -0500

    dining page

--- frontend/src/components/diningCard.jsx
+++ frontend/src/components/diningCard.jsx
@@ -1,3 +1,31 @@
-export default function DiningCard() {
-  return <></>;
+import PropTypes from "prop-types";
+import "./rideCard.css";
+
+export default function DiningCard({
+  diningImage,
+  diningName,
+  diningLocation,
+  diningProducts,
+  diningOverview,
+}) {
+  return (
+    <div className="card-container">
+      <div>
+        <img src={diningImage} alt="" />
+        <p>{diningOverview}</p>
+      </div>
+      <div className="content-container">
+        <h1>{diningName}</h1>
+        <p>{`Location: ${diningLocation}`}</p>
+        <p>{`Products: ${diningProducts}`}</p>
+      </div>
+    </div>
+  );
 }
+DiningCard.propTypes = {
+  diningImage: PropTypes.string,
+  diningName: PropTypes.string,
+  diningLocation: PropTypes.string,
+  diningProducts: PropTypes.string,
+  diningOverview: PropTypes.string,
+};
--- frontend/src/pages/Dining.jsx
+++ frontend/src/pages/Dining.jsx
@@ -1,3 +1,49 @@
+import Header from "../components/header";
+import DiningCard from "../components/diningCard";
+import diningImage from "../assets/images/placeholder-image.webp";
+
 export default function Dining() {
-  return <></>;
+  return (
+    <div>
+      <Header />
+      <h1>Restaurants & Dining</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ipsam porro
+        dignissimos dolore dolor maiores. Aut optio rem ipsa nostrum, provident,
+        obcaecati assumenda asperiores, eveniet dolore consectetur beatae! Illo,
+        fuga nesciunt?
+      </p>
+      <div>
+        <h2>Filter By Dining Type</h2>
+        <select name="dining" id="dining">
+          <option value="all">All Resturants</option>
+          <option value="kids">Mexican Food</option>
+          <option value="kids">Italian Food</option>
+          <option value="kids">Snacks & Beverages</option>
+          <option value="family">Desserts</option>
+        </select>
+      </div>
+      <DiningCard
+        diningImage={diningImage}
+        diningLocation={"123 santana drive"}
+        diningName={"Resturant Name 1"}
+        diningProducts={"products here"}
+        diningOverview={"burger and fries"}
+      />
+      <DiningCard
+        diningImage={diningImage}
+        diningLocation={"123 university lane"}
+        diningName={"Resturant Name 2"}
+        diningProducts={"products here"}
+        diningOverview={"ice cream"}
+      />
+      <DiningCard
+        diningImage={diningImage}
+        diningLocation={"123 hospital avenue"}
+        diningName={"Resturant Name 3"}
+        diningProducts={"products here"}
+        diningOverview={"hot dogs"}
+      />
+    </div>
+  );
 }
