b7526b05425be3cde6ab4ca59b26a1eb14b0d759
Darrenf040 <darrenfr83@gmail.com>
2024-10-20 22:39:09 -0500

    Added form to login page file

--- frontend/src/pages/AdminLogin.jsx
+++ frontend/src/pages/AdminLogin.jsx
@@ -1,19 +1,38 @@
-import Form from "../components/form";
+import { useState } from "react";
 import Header from "../components/header";
 import "./adminLogin.css";
 
 export default function AdminLogin() {
+  const [values, setValues] = useState({
+    userName: "",
+    password: "",
+  });
+  const handleSubmit = (e) => {};
   return (
     <div className="admin-page-container">
       <Header />
       <div className="admin-login-container">
-        <Form
-          heading="Admin Login"
-          subheading="Login with admin credentials"
-          label1Text="Email"
-          label2Text="Password"
-          submitText="Log In"
-        />
+        <div className="form-container">
+          <h1>Log In</h1>
+          <p>Log in with admin credentials</p>
+          <form onSubmit={handleSubmit}>
+            <label htmlFor="username">Username</label>
+            <input
+              onChange={(e) =>
+                setValues({ ...values, userName: e.target.value })
+              }
+              type="text"
+            />
+            <label htmlFor="password">Password</label>
+            <input
+              onChange={(e) =>
+                setValues({ ...values, password: e.target.value })
+              }
+              type="password"
+            />
+            <input type="submit" value="Submit" />
+          </form>
+        </div>
       </div>
     </div>
   );
