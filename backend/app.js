ac4b97e13228bd2fa2ae2eb1a77e4333131702cd
Darrenf040 <darrenfr83@gmail.com>
2024-10-20 22:09:29 -0500

    import authentication libraries

--- backend/app.js
+++ backend/app.js
@@ -2,12 +2,17 @@ const express = require("express");
 const cors = require("cors");
 const db = require("./connect");
 
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const cookieParser = require("cookie-parser");
+
 const app = express();
 
 //parses data that comes into json format
 app.use(express.json());
 
 app.use(cors());
+app.use(cookieParser());
 
 app.listen(3000, () => {
   console.log("server running");
