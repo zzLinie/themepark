49f88b493f8fc7853921aa5e186b2be834a199fb
Dat Huynh <huynhdat4977@gmail.com>
2024-10-27 23:10:35 -0500

    Edit Admin Report

--- frontend/src/pages/AdminReports.jsx
+++ frontend/src/pages/AdminReports.jsx
@@ -9,7 +9,15 @@ export default function AdminReports() {
       <div className="reports">
         <div className="popular-rides">Rides</div>
         <ReportCard reportHeading={"Customer visit"} />
-        <ReportCard reportHeading={"ride maintenance"} />
+        <ReportCard reportHeading={"Ride maintenance"} />
+        {/* Embed Looker Studio Report */}
+        <iframe
+          src="https://lookerstudio.google.com/embed/reporting/05d5374d-5f4f-4228-9272-962b49da6639/page/6zXD"
+          width="100%"
+          height="600px"
+          frameBorder="0"
+          allowFullScreen
+        ></iframe>
       </div>
     </div>
   );
