2aa7aae5bf0e94a27b91cf8ebab204b2d61481e1
Darrenf040 <darrenfr83@gmail.com>
2024-10-28 13:05:45 -0500

    removed reports

--- frontend/src/pages/AdminReports.jsx
+++ frontend/src/pages/AdminReports.jsx
@@ -1,16 +1,11 @@
 import "./adminReports.css";
-import ReportCard from "../components/reportCard";
 import AdminHeader from "../components/adminHeader";
 
 export default function AdminReports() {
   return (
     <div>
       <AdminHeader />
-      <div className="reports">
-        <div className="popular-rides">Rides</div>
-        <ReportCard reportHeading={"Customer visit"} />
-        <ReportCard reportHeading={"ride maintenance"} />
-      </div>
+      <div className="reports"></div>
     </div>
   );
 }
