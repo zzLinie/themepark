d2e92986f152874db706c22981a6f2c2ddaef639
Darrenf040 <darrenfr83@gmail.com>
2024-10-28 23:37:56 -0500

    fjdl

--- backend/connect.js
+++ backend/connect.js
@@ -8,7 +8,7 @@ const connection = mysql.createConnection({
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   port: Number(process.env.DB_PORT),
-  // ssl: { ca: fs.readFileSync(process.env.DB_SSL_PATH) },
+  ssl: { ca: process.env.DB_SSL_CERT },
 });
 
 connection.connect((err) => {
