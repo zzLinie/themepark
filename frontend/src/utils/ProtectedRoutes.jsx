c2d34e5a531569e5200fd730b157d24d8ddcf43a
Darrenf040 <darrenfr83@gmail.com>
2024-11-07 10:21:20 -0600

    redirect to login page for unauth users

--- frontend/src/utils/ProtectedRoutes.jsx
+++ frontend/src/utils/ProtectedRoutes.jsx
@@ -2,7 +2,7 @@ import PropTypes from "prop-types";
 import { Outlet, Navigate } from "react-router-dom";
 
 export default function ProtectedRoutes({ auth }) {
-  return auth ? <Outlet /> : <Navigate to="/admin" />;
+  return auth ? <Outlet /> : <Navigate to="/login" />;
 }
 
 ProtectedRoutes.propTypes = {
