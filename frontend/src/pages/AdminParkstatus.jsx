b4533329bf3b1ac3bba8e44f75a154899710a36d
linie <ringleadertheresa@gmail.com>
2024-11-13 15:34:56 -0600

    add more functionality to parkstatus page

--- frontend/src/pages/AdminParkstatus.jsx
+++ frontend/src/pages/AdminParkstatus.jsx
@@ -1,5 +1,5 @@
 import AdminHeader from "../components/adminHeader";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import axios from "axios";
 import Modal from "react-modal";
 import "./adminEmployees.css";
@@ -10,9 +10,10 @@ const ParkStatusForm = () => {
     parkStatusDate: "",
     weatherType: "",
   });
-
+  const modalRef = useRef(null);
   const [parkStatusList, setParkStatusList] = useState([]);
   const [editRow, setEditRow] = useState(null);
+  const [deleteState, setDeleteState] = useState(false);
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [parkHistoryList, setParkHistoryList] = useState([]);
   const [isHistoryModalOpen, setIsHistoryModalOpen] = useState(false);
@@ -22,8 +23,21 @@ const ParkStatusForm = () => {
     setParkStatusData({ ...ParkStatusData, [name]: value });
   };
 
+  const handleChildData = (dataObj) => {
+    setParkStatusData({ ...ParkStatusData, ...dataObj });
+  };
+
   const handleSubmit = async (e) => {
     e.preventDefault();
+    if(parkStatusList.includes(ParkStatusData.date)) {
+      const confirm = window.confirm(
+        "The date you are trying to submit for already exists"
+      );
+      if (!confirm) {
+        return;
+      }
+      return;
+    }
     if (ParkStatusData.weatherType === "2") {
       const confirm = window.confirm(
         "You are about to input this date as a RAINOUT. Double check the date before proceeding"
@@ -50,6 +64,18 @@ const ParkStatusForm = () => {
     axios
       .get("https://themepark-backend.onrender.com/parkstatus/read")
       .then((res) => setParkStatusList(res.data.result))
+      .catch((err) => console.error(err));
+  };
+
+  const getParkStatusData = (parkStatusID) => {
+    axios
+      .get(`https://themepark-backend.onrender.com/parkstatus/read/${parkStatusID}`)
+      .then((res) => {
+        setEditRow({ ...editRow, ...res.data.result });
+        setParkStatusData({ ...ParkStatusData, ...res.data.result[0] });
+        const modal = modalRef.current;
+        modal.showModal();
+      })
       .catch((err) => console.error(err));
   };
 
@@ -84,6 +110,31 @@ const ParkStatusForm = () => {
     return {};
   };
 
+  const getTimeStyle = (time) =>  {
+    if(time === null) {
+      return {color: "red"};
+    }
+    return {};
+  }
+
+  const getOpenTime = (openingTime) =>   {
+    switch(openingTime) {
+      case null:
+        return "CLOSED";
+        default:
+          return openingTime;
+    }
+  };
+
+  const getCloseTime = (closingTime) =>   {
+    switch(closingTime) {
+      case null:
+        return "CLOSED";
+        default:
+          return closingTime;
+    }
+  };
+
   const formatDate = (dateString) => {
     const date = new Date(dateString);
     return date.toLocaleDateString("en-TX");
@@ -107,12 +158,22 @@ const ParkStatusForm = () => {
     setIsHistoryModalOpen(false);
   };
 
-  const handleUpdate = async (e) => {
+
+  const handleUpdate = (e) => {
+    e.preventDefault();
+    axios
+      .put(`https://themepark-backend.onrender.com/parkstatus/update`, ParkStatusData)
+      .then((res) => alert(res.data))
+      .catch((err) => console.log(err));
+    setDeleteState(deleteState == true ? false : true);
+  };
+
+  /*const handleUpdate = async (e) => {
     e.preventDefault();
     try {
       const response = await axios.put(
         `https://themepark-backend.onrender.com/parkstatus/update`,
-        editRow
+        parkStatusData
       );
       if (response.data.message) {
         alert(response.data.message);
@@ -122,7 +183,7 @@ const ParkStatusForm = () => {
     } catch (err) {
       alert("Error: " + err.message);
     }
-  };
+  };*/
   
   return (
     <>
@@ -155,7 +216,42 @@ const ParkStatusForm = () => {
         </form>
       </div>
       <h2>Upcoming Park Days</h2>
-      <div className="tablecontainer">
+      <dialog ref={modalRef}>
+        <form>
+            <label>Date</label>
+            <input
+              type="date"
+              name="parkStatusDate"
+              value={parkStatusList.parkStatusDate}
+              onChange={(e) =>
+                setParkStatusData({ ...ParkStatusData, parkStatusDate: e.target.value })
+              }
+              required
+            />
+
+            <label>Weather Type</label>
+            <select
+              name="weatherType"
+              value={parkStatusList.weatherType}
+              onChange={(e) =>
+                setParkStatusData({ ...ParkStatusData, weatherType: e.target.value })
+              }
+              required
+            >
+              <option value="">Select Weather Type</option>
+              <option value="0">Fair</option>
+              <option value="1">Cloudy</option>
+              <option value="2">Rainout</option>
+            </select>
+
+          <button type="submit" onClick={handleUpdate}>
+            Update
+          </button>
+          <button type="button" onClick={() => modalRef.current.close()}>
+            Close
+          </button>
+        </form>
+      </dialog>
         <table>
           <thead>
             <tr>
@@ -177,12 +273,14 @@ const ParkStatusForm = () => {
                     {getWeatherDescription(val.weatherType)}
                   </td>
                   <td>{val.capacity}</td>
-                  <td>{val.openingTime}</td>
-                  <td>{val.closingTime}</td>
+                  <td style={getTimeStyle(val.openingTime)}>
+                    {getOpenTime(val.openingTime)}
+                  </td>
+                  <td style={getTimeStyle(val.closingTime)}>
+                    {getCloseTime(val.closingTime)}
+                  </td>
                   <div className="table-btn-container">
-                    <button onClick={() => openModal(val)}>
-                      Edit
-                    </button>
+                  <button onClick={() => getParkStatusData(val.parkStatusID)}>Edit</button>
                     </div>
                 </tr>
               </tbody>
@@ -227,7 +325,6 @@ const ParkStatusForm = () => {
         )}
         <button onClick={closeModal}>Close</button>
       </Modal>
-    </div>
     </>
   );
 };
