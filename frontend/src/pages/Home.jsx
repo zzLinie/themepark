59587e7b53b3f78b34a863e766f3e711eebb4729
hasitha454 <hasitha.vasireddy@gmail.com>
2024-11-17 21:21:07 +0530

    removed unwanted text

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -8,7 +8,7 @@ export default function Home() {
       <div className="hero-container">
       <div class="banner">
           <div class="scrolling-text">
-            Welcome to the  Themepark, where every corner is filled with thrills, magic, and excitement! whether you’re seeking adrenaline-pumping rides, immersive experiences, or family-friendly fun, AdventureWorld has something for everyone.
+            Welcome to the  Themepark, where every corner is filled with thrills, magic, and excitement! whether you’re seeking adrenaline-pumping rides, immersive experiences, or family-friendly fun, ThemePark has something for everyone.
           </div>
         </div>
       </div>
